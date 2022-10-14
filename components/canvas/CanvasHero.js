8f4ec38e52bb9a0539ae5a23095092113aa83500
Piotr <piotr.bania@europe.com>
2022-10-14 23:07:30 +0100

    init

--- components/canvas/CanvasHero.js
+++ components/canvas/CanvasHero.js
@@ -1,6 +1,9 @@
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
+import { Suspense } from 'react'
+import { Canvas, useLoader  } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
+
+import img from '../../public/logo.svg'
 
 const CanvasHero = () => {
 return (
@@ -8,16 +11,22 @@ return (
     {/* ---------------------- Canvas ---------------------- */}
     <Canvas className='canvasHero' style={{
             position: "absolute",
-            top: "25vh",
-            right: "25vw",
-            width: "50vw",
-            height: "50vh"
+            top: "0vh",
+            right: "0vw",
+            width: "100vw",
+            height: "100vh"
             }}>
         <ambientLight color="#FFFFFF" intensity={0.05} />
         <pointLight color="#D0D0D1" intensity={0.25} position={[-2, 0, 2]} />
         <directionalLight color="#697077" intensity={0.25} position={[-2, 0, 2]} />
 
         <OrbitControls autoRotate autoRotateSpeed={-0.25} />
+
+        <mesh>
+            <planeBufferGeometry attach="geometry" args={[2, 2]} />
+            <meshPhongMaterial attach="material" color="green" />
+        </mesh>
+
     </Canvas>
 </div>
 )
