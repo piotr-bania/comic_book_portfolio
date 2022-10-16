7d7cb6fded145ef423e3704a31fd858c04ffaf94
Piotr <piotr.bania@europe.com>
2022-10-16 21:28:33 +0100

    init

--- components/canvas/CanvasHero.js
+++ components/canvas/CanvasHero.js
@@ -2,7 +2,7 @@ import { Canvas  } from '@react-three/fiber'
 import { useFrame, OrbitControls } from '@react-three/drei'
 
 import Model from '../../public/models/Model'
-import Plane from '../../public/models/Plane'
+import Creative from '../../public/models/Creative'
 
 const CanvasHero = () => {
 return (
@@ -15,14 +15,14 @@ return (
                 width: "100vw",
                 height: "100vh"
                 }}>
-            <ambientLight color="#FFFFFF" intensity={0.05} />
-            <pointLight color="#D0D0D1" intensity={0.25} position={[-2, 0, 2]} />
-            <directionalLight color="#697077" intensity={0.25} position={[-2, 0, 2]} />
+            <ambientLight color="#FFFFFF" intensity={1} />
+            <pointLight color="#D0D0D1" intensity={1} position={[-2, 0, 2]} />
+            <directionalLight color="#697077" intensity={1} position={[-2, 0, 2]} />
 
-            <OrbitControls />
-            {/* <OrbitControls autoRotate autoRotateSpeed={-0.25} /> */}
+            <OrbitControls autoRotate autoRotateSpeed={-1} />
 
-            <Model/>                        
+            <Creative />
+
         </Canvas>
     </div>
 )}
